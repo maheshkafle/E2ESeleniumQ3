8ffb2f6619edc88f178d08dfa154b567c36412e2
MaheshKafle <mahesh.kafle@lisec.com>
2025-07-12 17:50:03 +0400

    Conditions

---
+++ src/Condition.java
@@ -0,0 +1,24 @@
+
+public class Condition {
+
+	public static void main(String[] args) 
+	{
+		//Print Multiple of 2 and remaining numbers too using condition in Java
+		
+		int[] numbers = {1,2,3,6,7,9,10,11,13, 16};
+		
+		for(int i=0; i<numbers.length;i++)
+		{
+			if(numbers[i]%2==0) 
+			{
+				System.out.println(numbers[i]+ " is multiple of 2");
+			}
+			
+			else 
+			{
+				System.out.println(numbers[i]);
+			}
+		}
+	}
+
+}
